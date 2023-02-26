fd9991367159bcb5d4f3ccc7b5e051d8ea2e1842
septemblue <focusramxram@gmail.com>
2023-02-26 20:01:55 +0700

    Session 1

---
+++ notes.md
@@ -0,0 +1,5 @@
+# Notes (this session) 
+- Create embedded server
+- Add application.yaml
+- Create custom Routing
+- Using install feature (application.kt)
--- src/main/kotlin/com/septemblue/Application.kt
+++ src/main/kotlin/com/septemblue/Application.kt
@@ -4,12 +4,22 @@ import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import com.septemblue.plugins.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
 
 fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
-        .start(wait = true)
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
+        install(Routing) {
+            myRouting()
+        }
+    }.start(wait = true)
 }
-
+fun Routing.myRouting() {
+        get("/") {
+            call.respond("Hello from myRouting")
+        }
+}
 fun Application.module() {
+//    val port = environment.config.propertyOrNull("ktor.deplyoment.port")?.getString() ?: "8080"
     configureRouting()
 }
